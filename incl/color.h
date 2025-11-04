5da0f94d07bb4c99fcd7a7d2a935ce2359ae0f7b
natomkul <154373686+natomkul@users.noreply.github.com>
2025-11-04 21:22:16 +0100

    Update color.h

--- incl/color.h
+++ incl/color.h
@@ -11,7 +11,7 @@ color ray_color(const ray& r, int depth, const hittable& world) {
 
     hit_record rec;
     if (world.hit(r, interval(0.001, infinity), rec)) {
-        vec direction = rec.normal + random_on_hemisphere();
+        vec direction = rec.normal + random_on_hemisphere(rec.normal);
         return 0.5 * ray_color(ray(rec.p, direction), depth-1, world);
         //return 0.5 * (rec.normal + color(1,1,1));
     }
