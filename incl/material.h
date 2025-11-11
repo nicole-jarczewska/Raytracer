17f2dbe49a02b973342b10664b83fd587c5b9486
natomkul <154373686+natomkul@users.noreply.github.com>
2025-11-11 17:36:37 +0100

    Update material.h

--- incl/material.h
+++ incl/material.h
@@ -12,3 +12,19 @@ class material {
       return false;
     }
 };
+
+class lambertian : public material {
+  public:
+    lambertian(const color& albedo) : albedo(albedo) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const {
+        auto scatter_direction = rec.normal + random_unit_vector();
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
+    }
+
+  private:
+    color albedo;
+};
