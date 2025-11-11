2f3a39941f47e8f45324c6bd1fd30e9a8b1a8168
natomkul <154373686+natomkul@users.noreply.github.com>
2025-11-11 15:41:02 +0100

    Update material.h

--- incl/material.h
+++ incl/material.h
@@ -1,7 +1,14 @@
 #pragma once
 #include "objects.h"
+#include "hit.h"
+
+class hit_record; 
 
 class material {
   public:
     virtual ~material()=default;
+
+    virtual bool scratter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered) const{
+      return false;
+    }
 };
