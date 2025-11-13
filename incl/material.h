4a05356f8040e6e708ab57e7e94b30af43005c70
natomkul <154373686+natomkul@users.noreply.github.com>
2025-11-13 17:32:39 +0100

    Update material.h

--- incl/material.h
+++ incl/material.h
@@ -32,3 +32,19 @@ class lambertian : public material {
   private:
     color albedo;
 };
+
+class metal : public material {
+  public:
+    metal(const color& albedo) : albedo(albedo) {}
+
+    bool scatter(const ray& r_in, const hit_record& rec, color& attenuation, ray& scattered)
+    const {
+        vec reflected = reflect(r_in.direction(), rec.normal);
+        scattered = ray(rec.p, reflected);
+        attenuation = albedo;
+        return true;
+    }
+
+  private:
+    color albedo;
+};
