e9522cd0c4c9cd25f63f8c8d4b7d165fd62a881c
natomkul <154373686+natomkul@users.noreply.github.com>
2025-11-15 18:13:25 +0100

    added vec refract

--- incl/vec.h
+++ incl/vec.h
@@ -133,3 +133,10 @@ inline vec random_in_unit_disk() {
 inline vec reflect(const vec& v, const vec& n) {
     return v - 2*dot(v,n)*n;
 }
+
+inline vec refract(const vec& uv, const vec& n, double etai_over_etat) {
+    auto cos_theta = std::fmin(dot(-uv, n), 1.0);
+    vec r_out_perp =  etai_over_etat * (uv + cos_theta*n);
+    vec r_out_parallel = -std::sqrt(std::fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
+}
